d285508de75f2290cf34b8874c5a011b4858e73b
DamianTivig <d.tivig@yahoo.com>
2026-06-18 13:14:57 +0300

    Update ssh_run.sh

--- ssh_run.sh
+++ ssh_run.sh
@@ -6,7 +6,7 @@ echo "Connecting to server..."
 ssh -tt -o StrictHostKeyChecking=no uie74356@10.198.127.171 "\
 export TERM=xterm && \
 source ~/.bash_profile 2>/dev/null || source ~/.profile 2>/dev/null || true && \
-cd /PROJ/db4/db/RDF/EU/2026Q1_new_server_SESP/EEU/2026R4_RDF_Europe_east_261H1/611/Scripts/ && \
-tcsh -c './LOAD_EEU_611.CSH |& tee LOAD_EEU_611_20.LOG'
+cd /PROJ/db4/db/RDF/NA/2026Q1_SeSp/2025R4_RDF_North_America_251H0/611/Scripts/ && \
+tcsh -c './LOAD_NA_611.CSH |& tee LOAD_NA_611_20.LOG'
 "
 ``
